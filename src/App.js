f18e1d8abf0ffc9e4ab24b150b580a4540c952af
mehradtoudekheil <mehradtoudekheil@gmail.com>
2024-12-18 14:47:59 +0330

    Start Project And Instal tools

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p className='bg-gray-900 text-sky-400'>hello</p>
+     <h1 className='text-red-400'>first</h1>
+     <i className="fa fa-user"></i> 
+    </div>
+  );
+}
+
+export default App;
